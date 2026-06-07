0c6425a8e0b1697f69f1dc3e53b82d2e91ed95da
Nikita Sychev <root@nsychev.ru>
2026-06-07 20:02:05 +0000

    Custom host/port

--- cf_token/cli.py
+++ cf_token/cli.py
@@ -1,8 +1,13 @@
+import click
 import uvicorn
 
 
-def main() -> None:
-    uvicorn.run("cf_token.app:app", host="0.0.0.0", port=8000)
+@click.command()
+@click.option("--host", default="0.0.0.0", help="Host to bind to.")
+@click.option("--port", default=8000, type=int, help="Port to bind to.")
+def main(*, host: str, port: int) -> None:
+    """Run the CF Access token web app."""
+    uvicorn.run("cf_token.app:app", host=host, port=port)
 
 
 if __name__ == "__main__":
